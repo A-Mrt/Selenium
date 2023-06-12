f0355272df845cba960779fd1c54418b92e776e1
A-Mrt <alexandra.mrotek@gmail.com>
2023-06-12 20:03:32 +0200

    Wejście na stronę, logowanie i wylogowanie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/aleksandramichnowicz/Desktop/repozytoria/Selenium/chromedriver/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.w3schools.com/xml/xpath_syntax.asp");
-        //*[@id="accept-choices"]
-        driver.findElement(By.xpath("//*[@id=\"accept-choices\"]")).click();
+        driver.get("https://demoqa.com/books");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("userName")).sendKeys("gunjankaushik");
+        driver.findElement(By.id("password")).sendKeys("Password@123");
+        driver.findElement(By.id("login")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        System.out.println("Jesteś na stronie" + driver.getCurrentUrl());
+        driver.quit();
     }
 }
