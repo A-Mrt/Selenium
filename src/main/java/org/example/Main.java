a5aec6d6216e57d82daf37fb39806ee430412ac2
A-Mrt <alexandra.mrotek@gmail.com>
2023-06-12 19:25:25 +0200

    Set up chromeriver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "/Users/aleksandramichnowicz/Desktop/repozytoria/Selenium/chromedriver/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.w3schools.com/xml/xpath_syntax.asp");
+        //*[@id="accept-choices"]
+        driver.findElement(By.xpath("//*[@id=\"accept-choices\"]")).click();
     }
 }
